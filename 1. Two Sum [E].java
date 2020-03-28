249c9a316cc3792f5d3bc27e83163cbda823757c
Srikant Sharma <59329658+srikant276sharma@users.noreply.github.com>
2020-03-28 15:06:41 +0530

    Update 1. Two Sum [E].java

--- 1. Two Sum [E].java
+++ 1. Two Sum [E].java
@@ -1,48 +1,33 @@
 1. Two Sum [E]
 https://leetcode.com/problems/two-sum/
+Kevin: https://www.youtube.com/watch?v=Aql6zHkONek
 
-/*
-Solution: 1
+/*Solution 1.
+ Using HashMap.
 */
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
-        }
-        return new int[0];
-    }
-}
---
-Complexity Analysis:
-Time complexity: O(n^2). For each element, we try to find its complement by looping through the rest of array which takes O(n) time. 
-                         Therefore, the time complexity is O(n^2).
-Space complexity : O(1).
-
-/*
-Solution: 2
-*/
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
         HashMap<Integer, Integer> indexMap = new HashMap<>();
-        int complement = 0;
-        for (int i = 0; i < nums.length; i++) {
-            complement = target - nums[i];
 
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
             if (indexMap.containsKey(complement)) {
-                return new int[]{indexMap.get(complement), i};
+                result[0] = indexMap.get(complement);
+                result[1] = i;
+                return result;
             } else {
                 indexMap.put(nums[i], i);
             }
         }
-        return new int[0];
+        return result;
     }
 }
+
 --
-Complexity Analysis:
 Time complexity: O(n). We traverse the list containing n elements only once. Each look up in the table costs only O(1) time.
 Space complexity: O(n). The extra space required depends on the number of items stored in the hash table, which stores at most n elements.
+--
+Asked by: Facebook, Google, Amazon, Airbnb, Microsoft, Uber, LinkedIn, Apple, Bloomberg, Microsoft, Alibaba, Adobe, and more.
 --
