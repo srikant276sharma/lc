07da1f4f8cd4a367975d5ee2397ae6a62ae55550
Srikant Sharma <59329658+srikant276sharma@users.noreply.github.com>
2020-04-06 00:47:30 +0530

    Create 70. Climbing Stairs [E].java

---
+++ 70. Climbing Stairs [E].java
@@ -0,0 +1,35 @@
+70. Climbing Stairs [E]
+https://leetcode.com/problems/climbing-stairs/
+Tushar: https://www.youtube.com/watch?v=CFQk7OQO_xM
+
+/* Solution 1:
+Using Dynamic Programming.
+Fornula: f(n) = f(n - 1) + f(n - 2)
+*/
+
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        int n1 = 1;
+        int n2 = 2;
+        int distinctWays = 0;
+
+        for (int i = 3; i <= n; i++) {
+            distinctWays = n1 + n2;
+            n1 = n2;
+            n2 = distinctWays;
+        }
+
+        return n2;
+    }
+}
+
+--
+Time complexity : O(n), Single pass.
+Space complexity: O(1), Constant space needed.
+--
+Asked by: Microsoft, Google, Alibaba, Bloomberg, Amazon, Goldman Sachs, Walmart Labs, LinkedIn.
+--
