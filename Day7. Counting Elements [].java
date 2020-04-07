f5ac5d21d81e5b9f54df6afe691d58f6feab0ad1
Srikant Sharma <59329658+srikant276sharma@users.noreply.github.com>
2020-04-08 00:16:43 +0530

    Create Day7. Counting Elements [].java

---
+++ Day7. Counting Elements [].java
@@ -0,0 +1,34 @@
+Day7. Counting Elements []
+https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3289/
+
+/* Solution 1:
+Using HashSet.
+*/
+
+class Solution {
+    public int countElements(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        HashSet<Integer> numSet = new HashSet<>();
+        for (int i : arr) {
+            numSet.add(i);
+        }
+        /*Count elements. */
+        int count = 0;
+        for (int i : arr) {
+            if (numSet.contains(i + 1)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
+--
+Time Complexity: O(n), where n is number of elements in the array.
+Space Complexity: O(n), where n is number of elements in the array.
+--
+Asked by:
+--
