f9ccfa808e4cacc94237a679185e02567dd69fc7
Srikant Sharma <59329658+srikant276sharma@users.noreply.github.com>
2020-05-30 19:56:02 +0530

    Create 371. Sum of Two Integers [E].java

---
+++ 371. Sum of Two Integers [E].java
@@ -0,0 +1,29 @@
+371. Sum of Two Integers [E]
+https://leetcode.com/problems/sum-of-two-integers/
+
+/* Solution 1: Using Bit operations.
+B2B: https://www.youtube.com/watch?v=qq64FrA2UXQ
+     https://github.com/bephrem1/backtobackswe/blob/master/Arrays%2C%20Primitives%2C%20Strings/AdditionWithOnlyBitshifting/AdditionWithOnlyBitshifting.java
+
+Time complexity: O(1). Considering integer has 32 bits.
+Space complexity: O(1).
+
+Asked by:
+*/
+
+class Solution {
+    public int getSum(int a, int b) {
+        //Keep adding until we have no carry left.
+        while (b != 0) {
+            /* Take note of what positions will need a carry, we will left shift this below and make b hold it. 
+               Remember: a carry is not applied where it is discovered. It is applied 1 position to the left of where it was born. */
+            int carry = a & b;
+            /* a's job is to keep the sum we are going to be working on, '^' does bit addition. */
+            a = a ^ b;
+            /* b will house the carry from the operation, we left shift by 1 because in the next iteration we will add against the carry. */
+            b = carry << 1;
+        }
+        /* Return a, it was used to house the running sum we were working on the whole time. */
+        return a;
+    }
+}
