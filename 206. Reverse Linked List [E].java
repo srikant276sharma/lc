8b08fc60a67201d64fbcc80b2c82316f453050b3
Srikant Sharma <59329658+srikant276sharma@users.noreply.github.com>
2020-06-04 23:16:36 +0530

    Update 206. Reverse Linked List [E].java

--- 206. Reverse Linked List [E].java
+++ 206. Reverse Linked List [E].java
@@ -7,11 +7,12 @@ While you are traversing the list,
 change the current node's next pointer to point to its previous element. 
 Since a node does not have reference to its previous node, you must store its previous element beforehand. 
 You also need another pointer to store the next node before changing the reference. 
-Do not forget to return the new head reference at the end! */
+Do not forget to return the new head reference at the end!
 
 TC: O(n). Assume that n is the list's length, the time complexity is O(n).
 SC: O(1).
-
+*/
+    
 /**
  * Definition for singly-linked list.
  * public class ListNode {
