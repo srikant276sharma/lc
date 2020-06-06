436c393d877656b0ba37f1ac3f2f0653303136f0
Srikant Sharma <59329658+srikant276sharma@users.noreply.github.com>
2020-06-07 01:50:54 +0530

    Update 79. Word Search [M].java

--- 79. Word Search [M].java
+++ 79. Word Search [M].java
@@ -4,8 +4,21 @@ https://leetcode.com/problems/word-search/
 /* Solution 1: Using Depth First Search.
 Kevin: https://www.youtube.com/watch?v=vYYNp0Jrdv0
 
-Time complexity: O(n), where n is number of cells in the grid.
-Space complexity: O(n), where n is number of cells in the grid.
+TC: O(N*4^L) where N is the number of cells in the board and L is the length of the word to be matched.
+    For the backtracking function, its execution trace would be visualized as a 4-ary tree, 
+    each of the branches represent a potential exploration in the corresponding direction. 
+    Therefore, in the worst case, the total number of invocation would be the number of nodes in a full 4-nary tree, 
+    which is about 4^L.
+
+    We iterate through the board for backtracking, i.e.
+    there could be N times invocation for the backtracking function in the worst case.
+
+    As a result, overall the time complexity of the algorithm would be O(N * 4^L).
+
+SC: O(L) where L is the length of the word to be matched.
+    The main consumption of the memory lies in the recursion call of the backtracking function. 
+    The maximum length of the call stack would be the length of the word. 
+    Therefore, the space complexity of the algorithm is O(L).
 
 Asked by: Amazon, Google, Facebook, Microsoft, Snapchat, Uber, Bloomberg, Apple, Zillow, Oracle, Quantcast, and Cruise Automation.
 */
