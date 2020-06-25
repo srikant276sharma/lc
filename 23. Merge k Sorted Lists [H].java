36a31c8ef57fe24c3dd929ef5eef99e69293d9dd
Srikant Sharma <59329658+srikant276sharma@users.noreply.github.com>
2020-06-26 01:14:36 +0530

    Update 23. Merge k Sorted Lists [H].java

--- 23. Merge k Sorted Lists [H].java
+++ 23. Merge k Sorted Lists [H].java
@@ -5,6 +5,19 @@ https://leetcode.com/problems/merge-k-sorted-lists/
 Using MinHeap.
 B2B: https://www.youtube.com/watch?v=ptYUCjfNhJY
 Amell: https://www.youtube.com/watch?v=tDn9O7UQ4E8
+
+TC: O(2(n * log(k))) = O(n * log(k))
+    Let n be the total elements across the k sorted arrays we are given.
+    Extracting and adding to the Min Heap will both take log(k) time.
+    For each of the n items, we will do an addition to the heap and removal from the heap (log(k) expense per heap operation).
+    Hence the times 2. It is dropped. We get O(n * log(k)).
+
+SC: O(k)
+    Our heap will need to hold k elements for most of this process and cannot worsen past this.
+
+We are not counting the size of the output array which uses O(n) space since that isn't core to our algorithm.
+
+Asked by: Facebook, Amazon, Google, Microsoft, Oracle, Bloomberg, IXL, Adobe, Uber, Wish, Apple, LinkedIn, VMWare, Yahoo, Walmart Labs, Salesforce, Samsung, and Mathworks.
 */
 
 /**
@@ -21,43 +34,25 @@ class Solution {
             return null;
         }
         /*MinHeap will contain at most K nodes. */
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((n1, n2) -> n1.val - n2.val);
-        /*Added only head of K lists. */
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((n1, n2) -> n1.val - n2.val);
         for (ListNode node : lists) {
             if (node != null)
-                pq.add(node);
+                minHeap.add(node);
         }
 
         ListNode dummy = new ListNode(0);
         ListNode current = dummy;
 
-        while (!pq.isEmpty()) {
-            ListNode listNode = pq.poll();
+        while (!minHeap.isEmpty()) {
+            ListNode listNode = minHeap.poll();
             current.next = listNode;
             current = current.next;
             /*Add next node to the MinHeap. */
             if (listNode.next != null) {
-                pq.add(listNode.next);
+                minHeap.add(listNode.next);
             }
         }
 
         return dummy.next;
     }
 }
-
---
-Complexities:
-
-Time: O(2(n * log(k))) = O(n * log(k))
-      Let n be the total elements across the k sorted arrays we are given.
-      Extracting and adding to the Min Heap will both take log(k) time.
-      For each of the n items, we will do an addition to the heap and removal from the heap (log(k) expense per heap operation).
-      Hence the times 2. It is dropped. We get O(n * log(k)).
-
-Space: O(k)
-       Our heap will need to hold k elements for most of this process and cannot worsen past this.
-
-We are not counting the size of the output array which uses O(n) space since that isn't core to our algorithm.
---
-Asked by: Facebook, Amazon, Google, Microsoft, Oracle, Bloomberg, IXL, Adobe, Uber, Wish, Apple, LinkedIn, VMWare, Yahoo, Walmart Labs, Salesforce, Samsung, and Mathworks.
---
