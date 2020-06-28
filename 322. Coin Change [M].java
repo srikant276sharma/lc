710e3e64897c1c5d4bf1ff616373c2bc9851d874
Srikant Sharma <59329658+srikant276sharma@users.noreply.github.com>
2020-06-29 00:23:57 +0530

    Update 322. Coin Change [M].java

--- 322. Coin Change [M].java
+++ 322. Coin Change [M].java
@@ -12,26 +12,22 @@ Space complexity: O(amount).
 
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        //minCoins[i] always holds min coins count we need for amount i.
-        int[] minCoins = new int[amount + 1];
-        minCoins[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            minCoins[i] = Integer.MAX_VALUE - 1;
-        }
+        //dp[i] always holds min coins count we need for amount i.
+        int[] dp = new int[amount + 1]; //Considering 0 as well so added one more element.
 
-        Arrays.sort(coins);
         for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                //if current amount is >= coin denomination then only we can use the coin.
-                if (i >= coins[j]) {
-                    //Min between, excluding the current coin or including the current coin.
-                    minCoins[i] = Math.min(minCoins[i], 1 + minCoins[i - coins[j]]);
-                } else {
-                    break;
+            int minCoin = amount + 1; //anything greater than amount.
+
+            for (int coin : coins) {
+                //if coin denomination is <= current amount then only we can use the coin.
+                if (coin <= i) {
+                    //Take minimum between, excluding the current coin and including the current coin.
+                    minCoin = Math.min(minCoin, 1 + dp[i - coin]);
                 }
             }
+            dp[i] = minCoin;
         }
 
-        return minCoins[amount] > amount ? -1 : minCoins[amount];
+        return (dp[amount] == amount + 1) ? -1 : dp[amount];
     }
 }
