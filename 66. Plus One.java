2ae6b6b20405f8a85184eb715428ea720f84c702
Srikant Sharma <59329658+srikant276sharma@users.noreply.github.com>
2020-07-07 00:31:20 +0530

    Update 66. Plus One.java

--- 66. Plus One.java
+++ 66. Plus One.java
@@ -1,10 +1,17 @@
-66. Plus One
+66. Plus One [E]
 https://leetcode.com/problems/plus-one/
---
+
+/* Solution 1: Using Math.
+
+TC: O(n), where n is length of input array.
+SC: O(n).
+*/
+
 class Solution {
     public int[] plusOne(int[] digits) {
-
-        for (int i = digits.length - 1; i >= 0; i--) {
+        //we can have 3 cases: 123, 129, 999.
+        int len = digits.length;
+        for (int i = len - 1; i >= 0; i--) {
             if (9 == digits[i]) {
                 digits[i] = 0;
             } else {
@@ -14,9 +21,8 @@ class Solution {
         }
 
         /*To handle a scenario where all digits are 9 like 99... */
-        int[] arr = new int[digits.length + 1];
-        arr[0] = 1;
-        return arr;
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
     }
 }
---
