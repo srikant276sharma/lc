8b8625efd9b24599c425e2386d9d9e6df3897443
Srikant Sharma <59329658+srikant276sharma@users.noreply.github.com>
2020-08-27 00:22:27 +0530

    Update and rename 412. Fizz Buzz.java to 412. Fizz Buzz [E].java

--- 412. Fizz Buzz [E].java
+++ 412. Fizz Buzz [E].java
@@ -1,7 +1,7 @@
-412. Fizz Buzz
+412. Fizz Buzz [E]
 https://leetcode.com/problems/fizz-buzz/
 
---Solution 1:
+/* Solution 1:
 Algorithm:
 1. Initialize an empty answer list.
 2. Iterate on the numbers from 1...N.
@@ -10,25 +10,28 @@ Algorithm:
 5. Else, Check if the number is divisible by 5, add Buzz.
 6. Else, add the number.
 
---
+TC: O(n), where n is the input.
+SC: O(n).
+*/
+
 class Solution {
     public List<String> fizzBuzz(int n) {
-        List<String> fbList = new ArrayList<>();
+        List<String> res = new ArrayList<>();
+        if (n < 1) {
+            return res;
+        }
+
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                fbList.add("FizzBuzz");
+            if (i % 15 == 0) {
+                res.add("FizzBuzz");
             } else if (i % 5 == 0) {
-                fbList.add("Buzz");
+                res.add("Buzz");
             } else if (i % 3 == 0) {
-                fbList.add("Fizz");
+                res.add("Fizz");
             } else {
-                fbList.add(Integer.toString(i));
+                res.add(String.valueOf(i));
             }
         }
-        return fbList;
+        return res;
     }
 }
---
-Time Complexity: O(N)
-Space Complexity: O(1)
---
