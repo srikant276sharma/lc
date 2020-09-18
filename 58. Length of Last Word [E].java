a4ac9c96a1b5195bc08760e232b9a499dd9c2894
Srikant Sharma <59329658+srikant276sharma@users.noreply.github.com>
2020-09-19 01:47:29 +0530

    Update 58. Length of Last Word [E].java

--- 58. Length of Last Word [E].java
+++ 58. Length of Last Word [E].java
@@ -4,7 +4,7 @@ https://leetcode.com/problems/length-of-last-word/
 /* Solution 1:
 
 Time complexity: O(n), where n is length of input string.
-Space complexity: O(m), where m is length of the last word.
+Space complexity: O(1).
 
 Asked by:
 */
