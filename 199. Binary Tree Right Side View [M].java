90175b9b6dcd1bb038f054207aeffb71ef2fba42
Srikant Sharma <59329658+srikant276sharma@users.noreply.github.com>
2020-10-15 02:30:35 +0530

    Create 199. Binary Tree Right Side View [M].java

---
+++ 199. Binary Tree Right Side View [M].java
@@ -0,0 +1,54 @@
+199. Binary Tree Right Side View [M]
+https://leetcode.com/problems/binary-tree-right-side-view/
+
+/* Solution 1: Using BFS.
+Amell: https://www.youtube.com/watch?v=ARhIumRrgYM
+
+TC: O(N), where N = number of nodes in the tree.
+SC: O(H), H = height of the tree, because of recursive call stack.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+                
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                if (i == levelSize - 1) {
+                    res.add(node.val);        
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return res;
+    }
+}
