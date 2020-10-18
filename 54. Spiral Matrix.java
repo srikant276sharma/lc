fcf85ed469e324bd5d65363cf20554f212756637
Srikant Sharma <59329658+srikant276sharma@users.noreply.github.com>
2020-10-18 15:59:04 +0530

    Update 54. Spiral Matrix.java

--- 54. Spiral Matrix.java
+++ 54. Spiral Matrix.java
@@ -8,35 +8,42 @@ https://leetcode.com/problems/spiral-matrix/
 
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> nums = new ArrayList<>();
-        if (matrix == null || matrix.length == 0) {
-            return nums;
+        List<Integer> res = new ArrayList<>();
+        //input check.
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return res;
         }
-        int top = 0;
-        int bottom = matrix.length - 1;
-        int left = 0;
-        int right = matrix[0].length - 1;
-        int size = matrix.length * matrix[0].length;
-
-        while (nums.size() < size) {
-            for (int i = left; i <= right && nums.size() < size; i++) {
-                nums.add(matrix[top][i]);
+        
+        int rows = matrix.length, cols = matrix[0].length;
+        int top = 0, right = cols - 1, bottom = rows - 1, left = 0;
+        
+        while (left <= right && top <= bottom) {
+            //top left to right processing.
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
             }
             top++;
-            for (int i = top; i <= bottom && nums.size() < size; i++) {
-                nums.add(matrix[i][right]);
+            //right top to bottom processing.
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][right]);
             }
             right--;
-            for (int i = right; i >= left && nums.size() < size; i--) {
-                nums.add(matrix[bottom][i]);
+            //bottom right to left processing.
+            if (left <= right && top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    res.add(matrix[bottom][i]);
+                }
+                bottom--;
             }
-            bottom--;
-            for (int i = bottom; i >= top && nums.size() < size; i--) {
-                nums.add(matrix[i][left]);
+            //left bottom to top processing.
+            if (left <= right && top <= bottom) {
+                for (int i = bottom; i >= top; i--) {
+                    res.add(matrix[i][left]);
+                }
+                left++;
             }
-            left++;
         }
-        return nums;
+        return res;
     }
 }
 
