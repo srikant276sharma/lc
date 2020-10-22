0ac0eb50fc912045a9e47f390658c5f12f5388d9
Srikant Sharma <59329658+srikant276sharma@users.noreply.github.com>
2020-10-23 01:16:09 +0530

    Update Count of subsets with sum equal to X [M].java

--- Count of subsets with sum equal to X [M].java
+++ Count of subsets with sum equal to X [M].java
@@ -11,33 +11,29 @@ SC: O(N * W)
 // "static void main" must be defined in a public class.
 public class Main {
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 1, 1, 1};
         Main main = new Main();
-        System.out.println(main.countSubsetsWithSum(nums, 1));
+        int[] nums = new int[]{1, 2, 3, 3};
+        System.out.println(main.countSubsetsWithSumX(nums, 6));
     }
     //
-    public int countSubsetsWithSum(int[] nums, int sum) {
+    public int countSubsetsWithSumX(int[] nums, int x) {
         int n = nums.length;
-        int[][] dp = new int[n + 1][sum + 1]; //because of 0, added an extra row and column.
         
-        //when item == 0 then we can't make any subset so setting 0.
-        for (int i = 1; i <= sum; i++) {
-            dp[0][i] = 0;
-        }
-        //when cur sum == 0. we can always have an empty set with it's element's sum == 0 so setting 1.
-        for (int j = 0; j <= n; j++) {
-            dp[j][0] = 1;
-        }
-        //Decision making.
-        for (int i = 1; i <= n; i++) { //num iteration.
-            for (int j = 1; j <= sum; j++) { //sum iteration.
-                if (nums[i - 1] > j) {
-                    dp[i][j] = dp[i - 1][j]; //exclude cur num.
-                } else {
-                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]]; //exclude or include cur num.
+        int[][] dp = new int[n + 1][x + 1]; //adding an extra row and column for 0.
+        
+        for (int i = 0; i <= n; i++) { //iterate over num.
+            for (int j = 0; j <= x; j++) { //iterate over sum.
+                if (j == 0) { //when cur sum == 0. we can always have an empty set with it's element's sum == 0 so setting 1.
+                    dp[i][j] = 1; 
+                } else if (i == 0) { //when item == 0 then we can't make any subset so setting 0.
+                    dp[i][j] = 0;
+                } else if (nums[i - 1] > j) { //num > cur sum: exclude
+                    dp[i][j] = dp[i - 1][j];
+                } else { //include or exclude num.
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
                 }
             }
         }
-        return dp[n][sum];
+        return dp[n][x];
     }
 }
