0b732a530f06eb9cb9f3ce9f81ea2abb255ad7dd
Srikant Sharma <59329658+srikant276sharma@users.noreply.github.com>
2021-03-23 09:34:17 +0530

    Create -Microsoft | OA 2019 | Largest K such that both K and -K exist in array.java

---
+++ -Microsoft | OA 2019 | Largest K such that both K and -K exist in array.java
@@ -0,0 +1,32 @@
+/* Solution 1:
+https://leetcode.com/discuss/interview-question/406031/
+
+Write a function that, given an array A of N integers, returns the lagest integer K > 0 such that both values K and -K exist in array A. 
+If there is no such integer, the function should return 0.
+
+Example 1:
+Input: [3, 2, -2, 5, -3]
+Output: 3
+
+Example 2:
+Input: [1, 2, 3, -4]
+Output: 0
+
+TC: O(n).
+SC: O(n).
+*/
+
+class Solution {
+    public int largestK(int[] nums) {
+        int res = 0;
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(-num);
+            if (set.contains(num)) {
+                res = Math.max(res, Math.abs(num));
+            }
+        }
+        return res;
+    }
+}
